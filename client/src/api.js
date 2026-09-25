0f314a56f96858548024e96e5269e6ea5b138a19
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2026-09-25 09:52:56 +0200

    ci: create unused variable

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,9 @@
 export const API_URL = 'http://localhost:4000/api'
 
 export const get = async (path) => {
+
+  const unused = 42
+
   const res = await fetch(`${API_URL}${path}`)
   if (!res.ok) throw new Error(`API svarade ${res.status}`)
   return res.json()
